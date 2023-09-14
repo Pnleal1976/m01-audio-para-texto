d62c4a9eae87b077c736d8bfe48362fa06f2ca8f
Thiago Arnaud <119428826+TicoArnaud@users.noreply.github.com>
2023-09-14 10:35:18 -0300

    Creating Managers and first view class using tkinter
    * Create SpeechToTextManager  to handle the speech-to-text work;
    * Create TranslatorManager  to handle the translator work;
    * Create the .gitignore file;
    * Create a MainMenu using Tkinter.
    ---------
    
    Co-authored-by: Sabiniano <sabiniano.maia@gmail.com>

---
+++ MainMenu.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from managers.SpeechToTextManager import SpeechToTextManager
+from managers.TranslatorManager import TranslatorManager
+
+def withoutCommand():
+    print("")
+
+def speechToTextFile():
+    stt = SpeechToTextManager()
+    filename = "audio_file_1.wav"
+    text = stt.convert_speech_to_text_file(audio_file_path=filename)
+    print("Recognized text: ", text)
+
+    mainTraductor = TranslatorManager()
+    mainTranslate = mainTraductor.translate(text, language_destiny='pt')
+    print(f"Original text: {text}")
+    print(f"Translated: {mainTranslate}")
+
+def speechToTextMicrophone():
+    stt = SpeechToTextManager()
+    text = stt.convert_speech_to_text_microphone(microphone_input=True)
+    print("Recognized text: ", text)
+
+    mainTraductor = TranslatorManager()
+    mainTranslate = mainTraductor.translate(text, language_destiny='en')
+    print(f"Original text: {text}")
+    print(f"Translated: {mainTranslate}")
+
+app = Tk()
+app.title("NExT-2023 - M01 Audio To Text")
+app.geometry("500x300")
+app.configure(background="#dde")
+
+menu = Menu(app)
+menuSpeechToText = Menu(menu, tearoff=0)
+menuSpeechToText.add_command(
+    label="From Audio File (wav)", command=speechToTextFile)
+menuSpeechToText.add_command(
+    label="From Microphne", command=speechToTextMicrophone)
+menuSpeechToText.add_separator()
+menuSpeechToText.add_command(label="Close", command=app.quit)
+menu.add_cascade(label="Speech To Text", menu=menuSpeechToText)
+
+menuTranslator = Menu(menu, tearoff=0)
+menuTranslator.add_command(
+    label="English to portuguese", command=withoutCommand)
+menuTranslator.add_command(
+    label="Portuguese to English", command=withoutCommand)
+menu.add_cascade(label="Translator", menu=menuTranslator)
+
+menuAbout = Menu(menu, tearoff=0)
+menuAbout.add_command(label="Speech to Text/Translator",
+                      command=withoutCommand)
+menu.add_cascade(label="About", menu=menuAbout)
+
+app.config(menu=menu)
+app.mainloop()
---
+++ managers/TranslatorManager.py
@@ -0,0 +1,14 @@
+from googletrans import Translator
+
+
+class TranslatorManager:
+    def __init__(self):
+        self.translator = Translator()
+
+    def translate(self, text, language_origin='auto', language_destiny='en'):
+        try:
+            traductor = self.translator.translate(
+                text, src=language_origin, dest=language_destiny)
+            return traductor.text
+        except Exception as e:
+            return f"Error in translation: {str(e)}"
